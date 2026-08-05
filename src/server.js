ef47c82005a8db54c5dd250785c6b6e73c96d2d5
santanajonatas106-dotcom <santanajonatas106@gmail.com>
2026-08-05 00:14:55 -0300

    Corrige armazenamento de sessões

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const PgSession = require('connect-pg-simple')(session);
+const pool = require('./db/pool');
 const helmet = require('helmet');
 const config = require('./config');
 const siteRoutes = require('./routes/site');
@@ -14,6 +16,11 @@ app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(session({
+    store: new PgSession({
+    pool,
+    tableName: 'user_sessions',
+    createTableIfMissing: true
+  }),
   name: 'curriculofacil.sid',
   secret: config.sessionSecret,
   resave: false,
