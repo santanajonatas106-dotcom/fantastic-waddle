9c0c7382b6502b2e2b0bd8afe6dfb98fa29d30a1
santanajonatas106-dotcom <santanajonatas106@gmail.com>
2026-08-08 15:19:01 -0300

    Increase file upload size limit to 5MB

--- src/routes/site.js
+++ src/routes/site.js
@@ -8,7 +8,7 @@ const { createPreference, getPayment, isApprovedForResume } = require('../servic
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 2 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (['image/jpeg', 'image/png', 'image/webp'].includes(file.mimetype)) {
       return cb(null, true);
